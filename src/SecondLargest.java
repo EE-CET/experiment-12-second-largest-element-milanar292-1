5cbeef615b31e173176a0e774503ea885f581944
milanar292 <milanar292@gmail.com>
2026-04-06 23:00:47 +0530

    Update SecondLargest.java

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -3,36 +3,25 @@ import java.util.Scanner;
 public class SecondLargest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-
-        for(int i = 0; i < N; i++) {
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-        if(N < 2) {
-            System.out.println("No second largest element");
-            return;
-        }
-
+        
         int largest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
-
-        for(int i = 0; i < N; i++) {
-            if(arr[i] > largest) {
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
                 secondLargest = largest;
                 largest = arr[i];
-            } 
-            else if(arr[i] > secondLargest && arr[i] != largest) {
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
                 secondLargest = arr[i];
             }
         }
-
-        if(secondLargest == Integer.MIN_VALUE) {
-            System.out.println("No second largest element");
-        } else {
-            System.out.println(secondLargest);
-        }
+        
+        System.out.println(secondLargest);
     }
 }
